ca88ac27362b6ae844a0d879e575b3cbc2a7fce0
Facundo Albri <144178778+Facualbri@users.noreply.github.com>
2024-09-14 16:53:46 -0300

    Add files via upload

---
+++ pizeria-fa.py
@@ -0,0 +1,16 @@
+ingreVege= "Pimiento o Tofu"
+ingreNoVege= "Peperoni, Jamon o Salmón"
+
+pizza= "mozzarella, tomate y"
+vege = input("Quieres una pizza vegetariana?: ").upper()
+
+if vege == "SI":
+    print(f"Elige un ingrediente \n{ingreVege}")
+    ingre= input("Ingresa el ingrediente: ")
+    print("Pizza vegetariana de", ingre)
+    print("Ingredientes:",pizza,ingre)
+elif vege == "NO":
+    print(f"Elige un ingrediente \n{ingreNoVege}")
+    ingre= input("Ingresa el ingrediente: ")
+    print("Pizza no vegetariana de", ingre)
+    print("Ingredientes:",pizza,ingre)
