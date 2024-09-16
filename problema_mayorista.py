e5692ff7c2c465ae0792312ef379fc60b55d99f3
Agustin Oria <agustinoria78@gmail.com>
2024-09-16 13:15:30 -0300

    Enunciado

---
+++ problema_mayorista.py
@@ -0,0 +1,21 @@
+# Don Roberto es dueño de un almacén mayorista en las afueras de la ciudad.
+# Su negocio ha crecido mucho en los últimos años,
+# y ahora vende una gran variedad de productos a pequeños comercios de la zona.
+# Como parte de su estrategia para atraer clientes y aumentar las ventas,
+# Don Roberto ha decidido implementar un sistema de descuentos según el
+# monto total de las compras de cada cliente.
+
+# Cada vez que un cliente hace un pedido, Don Roberto utiliza la cantidad del producto y 
+# el precio unitario para calcular el precio final, y asi puede aplicar diferentes 
+# descuentos para incentivar compras más grandes. Sin embargo, con el crecimiento del 
+# negocio, el cálculo manual de los descuentos comenzó a ser un problema, así que decidió 
+# automatizar el proceso.
+
+# Las reglas de descuento que Don Roberto utiliza son simples:
+
+# Sin descuento si el precio total de la compra es menor a 450.
+# 10% de descuento si el precio total está entre $450 y $1000.
+# 15% de descuento si el precio total supera los $1000.
+
+# inprimir en pantalla el precio final, el porcentaje de decuento que se le aplica
+# y el precio final con el descuento aplicado.
