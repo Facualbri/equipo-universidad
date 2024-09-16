f31b4156a93d48c1ed6f2b093d462929ff95c007
TomasGit12 <mercadotomas722@gmail.com>
2024-09-16 15:19:15 -0300

    Add files via upload

---
+++ Clasificacion.py
@@ -0,0 +1,12 @@
+edad = int(input("Ingresar tu edad: "))
+if edad <=2:
+    print(f"tu clasificacion es: infante") 
+elif  2 <= edad <=12:
+    print(f"tu clasificacion es: niño")
+elif 12 <= edad <=17:
+    print(f"tu clasificacion es: adolescente")
+elif 18 <= edad:
+    print(f"Tu clasificacion es: adulto")
+
+
+          
